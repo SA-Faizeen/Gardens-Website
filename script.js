09997366418347066d7eb3c0071948befc3c1b54
Syed-Faizeen <syedfaizeen2005@gmail.com>
2026-09-16 01:12:53 +0600

    add light/dark mode and hero section

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const navbar = document.getElementById("navbar")
-const openButton = document.getElementById("open-sidebar-button")
-
+const navbar = document.getElementById("navbar");
+const openButton = document.getElementById("open-sidebar-button");
+const overlay = document.getElementById("overlay");
 
 function openSidebar() {
-	navbar.classList.add("show")
-	openButton.setAttribute('aria-expanded', 'true')
+  navbar.classList.add("show");
+  overlay.classList.add("show");
+  openButton.setAttribute('aria-expanded', 'true');
 }
+
 function closeSidebar() {
-	navbar.classList.remove("show")
-	openButton.setAttribute('aria-expanded', 'false')
+  navbar.classList.remove("show");
+  overlay.classList.remove("show");
+  openButton.setAttribute('aria-expanded', 'false');
 }
